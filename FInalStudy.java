0369f27f624dee86b5a6c42e6e602799c7f95364
Ashish Kumar <104408789+ashisha162@users.noreply.github.com>
2023-06-20 08:55:01 +0530

    Add files via upload

---
+++ FInalStudy.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class A
+{
+final int x=5; //final variables  - Value once assingned cannot be changed
+
+final void fun()
+{
+System.out.println("Hi , I'm a function in class A");
+  }
+}
+class B extends A
+{
+//void fun() {
+
+//{
+//System.out.println(" I'm a function in B overrriding function in A");
+
+//}
+}
+
+public class FinalStudy
+{
+public static void main(String args[])
+{
+
+}
+}
